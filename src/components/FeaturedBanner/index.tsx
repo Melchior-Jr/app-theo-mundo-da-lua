3865b5b097abf3d734095f8e350d590b827bae04
Melchior-Jr <jamelchior72@gmail.com>
2026-04-05 06:40:57 -0400

    🐛 Fix build errors: Remove unused imports and components for production

--- src/components/FeaturedBanner/index.tsx
+++ src/components/FeaturedBanner/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Play } from 'lucide-react'
 import styles from './FeaturedBanner.module.css'
 
 interface FeaturedBannerProps {
